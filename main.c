d2f3f7080722212c8ecfed4c2f3619c9ce8c524b
Rinnegatamante <rinnegatamante@gmail.com>
2017-07-15 18:25:54 +0200

    Added two new modes, fixed a bug in REAR_OFF.
    Added two new modes, fixed a bug in REAR_OFF.

--- main.c
+++ main.c
@@ -2,19 +2,27 @@
 #include <taihen.h>
 #include "renderer.h"
 
-#define HOOKS_NUM 5
-#define MODES_NUM 6
+#define HOOKS_NUM 5 // Number of hooked functions
+#define MODES_NUM 8 // Number of available modes
 
+// Prototypes for sceTouch*2 funcs
+int sceTouchRead2(SceUInt32 port, SceTouchData *pData, SceUInt32 nBufs);
+int sceTouchPeek2(SceUInt32 port, SceTouchData *pData, SceUInt32 nBufs);
+
+// Macros for available modes
 enum{
-	DEFAULT = 0,
-	FRONT_ONLY = 1,
-	REAR_ONLY = 2,
-	SWAPPED = 3,
-	FRONT_OFF = 4,
-	REAR_OFF = 5
+	DEFAULT        = 0,
+	FRONT_ONLY     = 1,
+	REAR_ONLY      = 2,
+	SWAPPED        = 3,
+	FRONT_OFF      = 4,
+	REAR_OFF       = 5,
+	FRONT_OUT_ONLY = 6,
+	REAR_OUT_ONLY  = 7
 };
 
-static char* modes[] = {"Default", "Frontpad -> Rearpad", "Rearpad -> Frontpad", "Inverted pads", "Frontpad disabled", "Rearpad disabled"};
+// Textual names for available modes
+static char* modes[MODES_NUM] = {"Default", "Frontpad -> Rearpad", "Rearpad -> Frontpad", "Inverted pads", "Frontpad disabled", "Rearpad disabled", "Both -> Frontpad", "Both -> Rearpad"};
 
 static uint8_t current_hook = 0;
 static SceUID hooks[HOOKS_NUM];
@@ -23,6 +31,8 @@ static uint8_t mode = DEFAULT;
 static SceCtrlData pad;
 static uint32_t oldpad;
 static uint32_t mode_timer = 0;
+static SceTouchData t;
+static uint8_t not_fake = 1;
 
 void hookFunction(uint32_t nid, const void *func){
 	hooks[current_hook] = taiHookFunctionImport(&refs[current_hook],TAI_MAIN_MODULE,TAI_ANY_LIBRARY,nid,func);
@@ -56,6 +66,45 @@ int sceTouchRead_patched(SceUInt32 port, SceTouchData *pData, SceUInt32 nBufs){
 				ret = TAI_CONTINUE(int, refs[0], port, pData, nBufs);
 				pData->reportNum = 0;
 			}
+			break;
+		case FRONT_OUT_ONLY:
+			switch (port){
+				case SCE_TOUCH_PORT_BACK:
+					ret = TAI_CONTINUE(int, refs[0], port, pData, nBufs);
+					if (not_fake) pData->reportNum = 0;
+					break;
+				case SCE_TOUCH_PORT_FRONT:
+					ret = TAI_CONTINUE(int, refs[0], port, pData, nBufs);
+					not_fake = 0;
+					sceTouchRead(SCE_TOUCH_PORT_BACK, &t, nBufs);
+					not_fake = 1;
+					if (t.reportNum > 0){
+						memcpy(&pData->report[pData->reportNum], &t.report[0], (SCE_TOUCH_MAX_REPORT - pData->reportNum) * sizeof(SceTouchReport));
+						pData->reportNum += t.reportNum;
+						if (pData->reportNum > SCE_TOUCH_MAX_REPORT) pData->reportNum = SCE_TOUCH_MAX_REPORT;
+					}
+					break;
+			}
+			break;
+		case REAR_OUT_ONLY:
+			switch (port){
+				case SCE_TOUCH_PORT_FRONT:
+					ret = TAI_CONTINUE(int, refs[0], port, pData, nBufs);
+					if (not_fake) pData->reportNum = 0;
+					break;
+				case SCE_TOUCH_PORT_BACK:
+					ret = TAI_CONTINUE(int, refs[0], port, pData, nBufs);
+					not_fake = 0;
+					sceTouchRead(SCE_TOUCH_PORT_FRONT, &t, nBufs);
+					not_fake = 1;
+					if (t.reportNum > 0){
+						memcpy(&pData->report[pData->reportNum], &t.report[0], (SCE_TOUCH_MAX_REPORT - pData->reportNum) * sizeof(SceTouchReport));
+						pData->reportNum += t.reportNum;
+						if (pData->reportNum > SCE_TOUCH_MAX_REPORT) pData->reportNum = SCE_TOUCH_MAX_REPORT;
+					}
+					break;
+			}
+			break;
 		default:
 			ret = TAI_CONTINUE(int, refs[0], port, pData, nBufs);
 			break;
@@ -90,6 +139,45 @@ int sceTouchRead2_patched(SceUInt32 port, SceTouchData *pData, SceUInt32 nBufs){
 				ret = TAI_CONTINUE(int, refs[1], port, pData, nBufs);
 				pData->reportNum = 0;
 			}
+			break;
+		case FRONT_OUT_ONLY:
+			switch (port){
+				case SCE_TOUCH_PORT_BACK:
+					ret = TAI_CONTINUE(int, refs[1], port, pData, nBufs);
+					if (not_fake) pData->reportNum = 0;
+					break;
+				case SCE_TOUCH_PORT_FRONT:
+					ret = TAI_CONTINUE(int, refs[1], port, pData, nBufs);
+					not_fake = 0;
+					sceTouchRead2(SCE_TOUCH_PORT_BACK, &t, nBufs);
+					not_fake = 1;
+					if (t.reportNum > 0){
+						memcpy(&pData->report[pData->reportNum], &t.report[0], (SCE_TOUCH_MAX_REPORT - pData->reportNum) * sizeof(SceTouchReport));
+						pData->reportNum += t.reportNum;
+						if (pData->reportNum > SCE_TOUCH_MAX_REPORT) pData->reportNum = SCE_TOUCH_MAX_REPORT;
+					}
+					break;
+			}
+			break;
+		case REAR_OUT_ONLY:
+			switch (port){
+				case SCE_TOUCH_PORT_FRONT:
+					ret = TAI_CONTINUE(int, refs[1], port, pData, nBufs);
+					if (not_fake) pData->reportNum = 0;
+					break;
+				case SCE_TOUCH_PORT_BACK:
+					ret = TAI_CONTINUE(int, refs[1], port, pData, nBufs);
+					not_fake = 0;
+					sceTouchRead2(SCE_TOUCH_PORT_FRONT, &t, nBufs);
+					not_fake = 1;
+					if (t.reportNum > 0){
+						memcpy(&pData->report[pData->reportNum], &t.report[0], (SCE_TOUCH_MAX_REPORT - pData->reportNum) * sizeof(SceTouchReport));
+						pData->reportNum += t.reportNum;
+						if (pData->reportNum > SCE_TOUCH_MAX_REPORT) pData->reportNum = SCE_TOUCH_MAX_REPORT;
+					}
+					break;
+			}
+			break;
 		default:
 			ret = TAI_CONTINUE(int, refs[1], port, pData, nBufs);
 			break;
@@ -124,6 +212,45 @@ int sceTouchPeek_patched(SceUInt32 port, SceTouchData *pData, SceUInt32 nBufs){
 				ret = TAI_CONTINUE(int, refs[2], port, pData, nBufs);
 				pData->reportNum = 0;
 			}
+			break;
+		case FRONT_OUT_ONLY:
+			switch (port){
+				case SCE_TOUCH_PORT_BACK:
+					ret = TAI_CONTINUE(int, refs[2], port, pData, nBufs);
+					if (not_fake) pData->reportNum = 0;
+					break;
+				case SCE_TOUCH_PORT_FRONT:
+					ret = TAI_CONTINUE(int, refs[2], port, pData, nBufs);
+					not_fake = 0;
+					sceTouchPeek(SCE_TOUCH_PORT_BACK, &t, nBufs);
+					not_fake = 1;
+					if (t.reportNum > 0){
+						memcpy(&pData->report[pData->reportNum], &t.report[0], (SCE_TOUCH_MAX_REPORT - pData->reportNum) * sizeof(SceTouchReport));
+						pData->reportNum += t.reportNum;
+						if (pData->reportNum > SCE_TOUCH_MAX_REPORT) pData->reportNum = SCE_TOUCH_MAX_REPORT;
+					}
+					break;
+			}
+			break;
+		case REAR_OUT_ONLY:
+			switch (port){
+				case SCE_TOUCH_PORT_FRONT:
+					ret = TAI_CONTINUE(int, refs[2], port, pData, nBufs);
+					if (not_fake) pData->reportNum = 0;
+					break;
+				case SCE_TOUCH_PORT_BACK:
+					ret = TAI_CONTINUE(int, refs[2], port, pData, nBufs);
+					not_fake = 0;
+					sceTouchPeek(SCE_TOUCH_PORT_FRONT, &t, nBufs);
+					not_fake = 1;
+					if (t.reportNum > 0){
+						memcpy(&pData->report[pData->reportNum], &t.report[0], (SCE_TOUCH_MAX_REPORT - pData->reportNum) * sizeof(SceTouchReport));
+						pData->reportNum += t.reportNum;
+						if (pData->reportNum > SCE_TOUCH_MAX_REPORT) pData->reportNum = SCE_TOUCH_MAX_REPORT;
+					}
+					break;
+			}
+			break;
 		default:
 			ret = TAI_CONTINUE(int, refs[2], port, pData, nBufs);
 			break;
@@ -158,6 +285,45 @@ int sceTouchPeek2_patched(SceUInt32 port, SceTouchData *pData, SceUInt32 nBufs){
 				ret = TAI_CONTINUE(int, refs[3], port, pData, nBufs);
 				pData->reportNum = 0;
 			}
+			break;
+		case FRONT_OUT_ONLY:
+			switch (port){
+				case SCE_TOUCH_PORT_BACK:
+					ret = TAI_CONTINUE(int, refs[3], port, pData, nBufs);
+					if (not_fake) pData->reportNum = 0;
+					break;
+				case SCE_TOUCH_PORT_FRONT:
+					ret = TAI_CONTINUE(int, refs[3], port, pData, nBufs);
+					not_fake = 0;
+					sceTouchPeek2(SCE_TOUCH_PORT_BACK, &t, nBufs);
+					not_fake = 1;
+					if (t.reportNum > 0){
+						memcpy(&pData->report[pData->reportNum], &t.report[0], (SCE_TOUCH_MAX_REPORT - pData->reportNum) * sizeof(SceTouchReport));
+						pData->reportNum += t.reportNum;
+						if (pData->reportNum > SCE_TOUCH_MAX_REPORT) pData->reportNum = SCE_TOUCH_MAX_REPORT;
+					}
+					break;
+			}
+			break;
+		case REAR_OUT_ONLY:
+			switch (port){
+				case SCE_TOUCH_PORT_FRONT:
+					ret = TAI_CONTINUE(int, refs[3], port, pData, nBufs);
+					if (not_fake) pData->reportNum = 0;
+					break;
+				case SCE_TOUCH_PORT_BACK:
+					ret = TAI_CONTINUE(int, refs[3], port, pData, nBufs);
+					not_fake = 0;
+					sceTouchPeek2(SCE_TOUCH_PORT_FRONT, &t, nBufs);
+					not_fake = 1;
+					if (t.reportNum > 0){
+						memcpy(&pData->report[pData->reportNum], &t.report[0], (SCE_TOUCH_MAX_REPORT - pData->reportNum) * sizeof(SceTouchReport));
+						pData->reportNum += t.reportNum;
+						if (pData->reportNum > SCE_TOUCH_MAX_REPORT) pData->reportNum = SCE_TOUCH_MAX_REPORT;
+					}
+					break;
+			}
+			break;
 		default:
 			ret = TAI_CONTINUE(int, refs[3], port, pData, nBufs);
 			break;
@@ -171,11 +337,11 @@ int sceDisplaySetFrameBuf_patched(const SceDisplayFrameBuf *pParam, int sync) {
 	if (((pad.buttons & SCE_CTRL_CROSS) && (pad.buttons & SCE_CTRL_TRIANGLE) && (pad.buttons & SCE_CTRL_START)) && (!((oldpad & SCE_CTRL_CROSS) && (oldpad & SCE_CTRL_TRIANGLE) && (oldpad & SCE_CTRL_START)))){
 		mode = (mode + 1) % MODES_NUM;
 		mode_timer = 100;
+		setTextColor(0x00FFFFFF);
 	}
 	oldpad = pad.buttons;
 	
 	if (mode_timer > 0){
-		setTextColor(0x00FFFFFF);
 		drawStringF(5, 5, "Touch Inverter mode: %s", modes[mode]);
 		mode_timer--;
 	}
@@ -186,7 +352,7 @@ int sceDisplaySetFrameBuf_patched(const SceDisplayFrameBuf *pParam, int sync) {
 void _start() __attribute__ ((weak, alias ("module_start")));
 int module_start(SceSize argc, const void *args) {
 	
-	// Hooking touch functions
+	// Hooking touch and display functions
 	hookFunction(0x169A1D58, sceTouchRead_patched);
 	hookFunction(0x39401BEA, sceTouchRead2_patched);
 	hookFunction(0xFF082DF0, sceTouchPeek_patched);
